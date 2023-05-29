a45c7285facdf6f11ef689e8fc707f235b6d2a6c
Hollister009 <reactive.andrew@yahoo.com.ua>
2023-05-29 21:15:13 +0300

    CustomModel cleanup

--- src/components/Scene/components/CustomModel.jsx
+++ src/components/Scene/components/CustomModel.jsx
@@ -1,12 +1,11 @@
-import { useRef, useState} from 'react';
-import { useFrame,useLoader } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
 import { MeshStandardMaterial, Object3D } from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 export default function CustomModel(props) {
   const meshRef = useRef();
-  const instanceMeshRef = useRef();
   const [rotate, setRotate] = useState();
 
   // Load the OBJ model using the useLoader hook
@@ -14,33 +13,28 @@ export default function CustomModel(props) {
 
   obj.traverse((child) => {
     if (child instanceof Object3D) {
-      child.material = new MeshStandardMaterial({ color: 'red' })
+      child.material = new MeshStandardMaterial({ color: 'red' });
     }
-  })
+  });
 
-  // Set the mesh to the loaded object
   meshRef.current = obj;
 
   // Use the useFrame hook to update the rotation of the mesh every frame
   useFrame(() => {
-    const instanceMesh = instanceMeshRef.current
-
-    if (rotate && instanceMesh) {
-      instanceMesh.rotation.y += 0.01;
+    if (rotate) {
+      meshRef.current.rotation.y += 0.01;
     }
   });
 
   return (
     <RigidBody type="fixed">
-      <instancedMesh
-        ref={instanceMeshRef}
-        {...props}
+      <primitive
+        object={meshRef.current}
         onPointerDown={() => {
           setRotate((prevState) => !prevState);
         }}
-      >
-        <primitive object={meshRef.current} />
-      </instancedMesh>
+        {...props}
+      />
     </RigidBody>
   );
 }
