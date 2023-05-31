db4d9618f289bc649afa46f7243bf8dd76f031dc
Vadym Kobylan <vadym.kobylan@gmail.com>
2023-06-01 01:08:46 +0300

    axiosInstance url

--- src/redux/axiosInstance.js
+++ src/redux/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080';
+const baseURL = 'http://localhost:8443';
 
 // Create a new instance of Axios
 export const getInstance = axios.create({
